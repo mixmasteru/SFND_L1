354bb02a2af06d615c2fd66fb793947d8aa3df2f
Ulrich Pech <ulrichpech@gmail.com>
2021-01-15 13:39:00 +0100

    clean up

--- src/environment.cpp
+++ src/environment.cpp
@@ -145,7 +145,7 @@ int main(int argc, char **argv) {
     //simpleHighway(viewer);
 
     ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
-    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_2");
+    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
     auto streamIterator = stream.begin();
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
 
